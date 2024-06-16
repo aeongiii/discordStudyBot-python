b4ac26c09e8d786b662f6f308588381e8c1886c9
aeongiii <ljhkys6874@naver.com>
2024-06-16 17:14:58 +0900

    springboot project 시작

---
+++ studyBot-Springboot/src/main/java/com/yeeun/studyBot_Springboot/StudyBotSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.yeeun.studyBot_Springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyBotSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyBotSpringbootApplication.class, args);
+	}
+
+}
---
+++ studyBot-Springboot/src/test/java/com/yeeun/studyBot_Springboot/StudyBotSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yeeun.studyBot_Springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyBotSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
